7baccdd3c77c2522884e4dd25c45810b2ffe5bfe
Your Name <you@example.com>
2026-05-18 06:36:40 +0530

    Complete Invigil deployment and GUI upgrade

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 
-BASE_DIR = os.path.dirname(
-    os.path.abspath(__file__)
-)
+if getattr(sys, 'frozen', False):
+
+    BASE_DIR = os.path.dirname(
+        sys.executable
+    )
+
+else:
+
+    BASE_DIR = os.path.dirname(
+        os.path.abspath(__file__)
+    )
 
 dotenv_path = os.path.join(
     BASE_DIR,
